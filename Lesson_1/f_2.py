c1ebf4c17158055b17beb21eceda51c9b6a3f364
Igor_Acer <i_gor88@mail.ru>
2021-01-31 13:25:21 +0700

    Домашнее задание №1

---
+++ Lesson_1/f_2.py
@@ -0,0 +1,11 @@
+word_1 = b"class"
+word_2 = b"function"
+word_3 = b"method"
+
+print(type(word_1))
+print(len(word_1))
+print(type(word_2))
+print(len(word_2))
+print(type(word_3))
+print(len(word_3))
+print(word_1, word_2, word_3)
